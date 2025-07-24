d11d2668a5cbaadf914f503f125ef74f35800c8c
laraproto <laraproto@proton.me>
2025-07-24 20:30:13 +0300

    test fixing subdomain routing and submit caddyfile

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,6 +4,9 @@ import { PUBLIC_URL } from '$env/static/public';
 export const reroute: Reroute = async ({ url }) => {
 	const split = url.hostname.split('.');
 
+	console.log(url.hostname);
+	console.log(split);
+
 	if (url.hostname !== PUBLIC_URL && split.length >= 2) {
 		return `/${split[0]}${url.pathname}`;
 	}
