ab9a289e8c9312768ab85939be8aac1ceb6c56db
laraproto <laraproto@proton.me>
2025-07-31 14:21:22 +0300

    some cleanup

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,13 +6,17 @@
 		Logo art by <a href="https://bsky.app/profile/did:plc:sqwkomf6gkzbhnctiqexepzx">Aegiscarr</a>
 	</p>
 	<p>
-		&copy; {new Date().getFullYear()} <a href="https://laratheprotogen.dev">Lara The Protogen</a>.
-		All rights reserved. Meow
+		{new Date().getFullYear()} Project made by
+		<a href="https://laratheprotogen.dev">Lara The Protogen</a>.
+		<a href="https://github.com/laraproto/bsky-handles-service">View source code here</a>. Meow
 	</p>
 </footer>
 
 <style lang="postcss">
 	@reference "tailwindcss";
+	a {
+		@apply hover:underline hover:text-sky-500 transition;
+	}
 	.footer {
 		@apply absolute bottom-0 z-10 w-full p-4 text-center;
 	}
